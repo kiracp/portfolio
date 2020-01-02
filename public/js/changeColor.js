cbafab926ed042b55d24050aecb58f1a7603437e
Kira Prentice <kprentice@Kiras-MacBook-Air.local>
2020-01-02 15:40:54 -0500

    remix website with new content and colors

---
+++ public/js/changeColor.js
@@ -0,0 +1,44 @@
+var url = "http://colormind.io/api/";
+var data = {
+	model : "default",
+	input : [[44,43,44],[116,84,78],"N","N","N","N","N"]
+}
+
+function getColors() {
+	var http = new XMLHttpRequest();
+
+	http.onreadystatechange = function() {
+		if(http.readyState == 4 && http.status == 200) {
+			var palette = JSON.parse(http.responseText).result;
+			console.log(palette);
+
+			setColors(palette);
+		}
+	}
+
+	http.open("POST", url, true);
+	http.send(JSON.stringify(data));
+
+}
+
+function setColors(palette) {
+	console.log(palette.length);
+
+	var bgColors = 5;
+
+	for (var i=1; i<=bgColors; i++) {
+		var className = "bg-" + i;
+
+		var color = "rgb(" + palette[i-1][0] + "," + palette[i-1][1] + "," + palette[i-1][2] + ")";
+		console.log(color);
+
+		var colorClasses = document.getElementsByClassName(className);
+		for (let item of colorClasses) {
+		    item.style.backgroundColor = color;
+		}
+	}
+	// var document.getElementsByClassName("bg-1");
+}
+
+console.log("called");
+getColors();
